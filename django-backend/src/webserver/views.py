5d256e3447caee9a0b064a9a2ad69a3a0920a46c
SkarioT <vlad.skariot@gmail.com>
2021-10-21 18:37:35 +0300

    webserver_fix_2

--- django-backend/src/webserver/views.py
+++ django-backend/src/webserver/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def test_req():
+def test_req(request):
     return HttpResponse("<h1>test msg</h1>")
